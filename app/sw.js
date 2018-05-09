02ea10b535109532853792b2461a4cfb2b8e9639
webruin <tim.smith.hdg@gmail.com>
2018-05-09 04:51:17 -0700

    sw

--- app/sw.js
+++ app/sw.js
@@ -4,9 +4,8 @@
 //Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   const indexPage = new Request('index.html');
-  const pageStyle = new Request('./assets/css/style.min.css');
   event.waitUntil(
-    fetch(indexPage, pageStyle).then(function(response) {
+    fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
         console.log('[PWA Builder] Cached index page during Install'+ response.url);
         return cache.put(indexPage, response);
